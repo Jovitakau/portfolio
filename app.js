812519297eb310fe5a3294aeb843aa8786ba5329
crisostomi <donatocrisostomi@gmail.com>
2021-03-31 22:36:30 +0200

    ready to deploy

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+const router = express.Router();
+
+app.use(express.static('public'));
+app.set('views', 'public/views'); 
+
+// define the home page route
+router.get('/', function(req, res) {
+  res.render("index.html");
+});
+
+router.get('/projects', function(req, res) {
+  res.render('sections/all.html');
+});
+
+router.get('/industrial', function(req, res) {
+  res.render('sections/industrial.html');
+});
+
+router.get('/service', function(req, res) {
+  res.render('sections/service.html');
+});
+
+router.get('/about', function(req, res) {
+  res.render('about/about.html');
+});
+
+router.get('/projects/:proj', function(req, res) {
+  res.render('projects/'+req.params.proj);
+});
+
+app.use(bodyParser.json());
+app.use(router)
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
+
